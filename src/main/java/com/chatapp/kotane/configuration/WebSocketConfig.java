98a369528967a7f9b7e5e88de077988b78edbedd
allen <boseallen192@gmail.com>
2025-03-19 19:02:54 +0530

    need to fix error

--- src/main/java/com/chatapp/kotane/configuration/WebSocketConfig.java
+++ src/main/java/com/chatapp/kotane/configuration/WebSocketConfig.java
@@ -7,21 +7,9 @@ import org.springframework.context.annotation.*;
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
-    private final SignalHandler _signalHandler;
-
-    public WebSocketConfig(SignalHandler signalHandler) {
-        _signalHandler = signalHandler;
-    }
-
-    // @Override
-    // public void registerWebSocketHandlers(WebSocketHandlerRegistry
-    // handlerRegistry) {
-    // handlerRegistry.addHandler(new ConnectionHandler(),
-    // "/chat").setAllowedOrigins("*");
-    // }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(_signalHandler, "/broadcast").setAllowedOrigins("*");
+        registry.addHandler(new SignalHandler(), "/broadcast").setAllowedOrigins("*");
     }
 }
--- src/main/java/com/chatapp/kotane/handlers/SignalHandler.java
+++ src/main/java/com/chatapp/kotane/handlers/SignalHandler.java
@@ -3,12 +3,37 @@ package com.chatapp.kotane.handlers;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.handler.*;
 import org.springframework.web.socket.*;
-import java.util.*;
-import java.util.concurrent.*;;
+import java.util.concurrent.*;
 
 @Component
 public class SignalHandler extends TextWebSocketHandler {
 
-    private final Map<String, WebSocketSession> webSocketSessions = new ConcurrentHashMap<>();
+    private static CopyOnWriteArrayList<WebSocketSession> sessions = new CopyOnWriteArrayList<WebSocketSession>();
 
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        super.afterConnectionEstablished(session);
+
+        sessions.add(session);
+        session.sendMessage(new TextMessage("connected by " + session.getId()));
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        super.afterConnectionClosed(session, status);
+
+        sessions.remove(session);
+        System.out.println("disconnected by " + session.getId());
+    }
+
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        super.handleTextMessage(session, message);
+
+        for (WebSocketSession s : sessions) {
+            if (s.isOpen() && s.getId() != session.getId()) {
+                session.sendMessage(message);
+            }
+        }
+    }
 }
